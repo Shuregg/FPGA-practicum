f91dac10aabf34d20b404e2ca1aac83306760800
Shuregg <san4ows55@gmail.com>
2024-04-08 01:18:39 +0300

    rename file, add clock gating

--- lab5_pipelines_and_systolic_arrays/rtl/pow5_pipelined_valid_mod.sv
+++ lab5_pipelines_and_systolic_arrays/rtl/pow5_pipelined_valid_mod.sv
@@ -36,6 +36,19 @@ module pow5_pipelined_valid_mod
   logic data_valid_stage_3_ff;
   logic output_valid_ff;
 
+  logic pow_input_en_0;
+  logic pow_input_en_1;
+  logic pow_input_en_2;
+  logic pow_input_en_3;
+
+  logic gated_clk_0;
+  logic gated_clk_1;
+  logic gated_clk_2;
+  logic gated_clk_3;
+  
+
+
+  logic 
 
     // "Valid" flags
   always_ff @ (posedge clk_i or posedge rst_i)
@@ -68,30 +81,39 @@ module pow5_pipelined_valid_mod
     // Exercise: 1) remove unnecessary resets here to reduce ASIC area
     //           2) use clock gating to reduce pipeline power consumption
 
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_input_ff <= '0;
-    else
+  // Enable signals for pow_input flip-flops
+  assign pow_input_en_0 = input_valid_ff;
+  assign pow_input_en_1 = data_valid_stage_1_ff;
+  assign pow_input_en_2 = data_valid_stage_2_ff;
+  assign pow_input_en_3 = data_valid_stage_3_ff;
+
+  assign gated_clk_0 = clk_i | ~pow_input_en_0;
+  assign gated_clk_1 = clk_i | ~pow_input_en_1;
+  assign gated_clk_2 = clk_i | ~pow_input_en_2;
+  assign gated_clk_3 = clk_i | ~pow_input_en_3;
+
+  always_ff @ (posedge gated_clk_0)
+    // if(pow_input_en_0)
       pow_input_ff <= pow_data_i;
+    // else
+      // pow_input_ff <= pow_input_ff;
 
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_input_stage_1_ff <= '0;
-    else
+  always_ff @ (posedge gated_clk_1)
+    // if(pow_input_en_1)
       pow_input_stage_1_ff <= pow_input_ff;
-
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_input_stage_2_ff <= '0;
-    else
+    // else
+      // pow_input_stage_1_ff <= pow_input_stage_1_ff;
+  
+  always_ff @ (posedge gated_clk_2)
+    // if(pow_input_en_2)
       pow_input_stage_2_ff <= pow_input_stage_1_ff;
-
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_input_stage_3_ff <= '0;
-    else
+    // else
+      // pow_input_stage_2_ff <= pow_input_stage_2_ff;
+  always_ff @ (posedge gated_clk_3)
+    // if(pow_input_en_3)
       pow_input_stage_3_ff <= pow_input_stage_2_ff;
-
+    // else
+    //   pow_input_stage_3_ff <= pow_input_stage_3_ff;
 
     // Multiply numbers
     assign pow_mul_stage_1 = pow_input_ff        * pow_input_ff;
@@ -102,32 +124,30 @@ module pow5_pipelined_valid_mod
 
     // Exercise: 1) remove unnecessary resets here to reduce ASIC area
     //           2) use clock gating to reduce pipeline power consumption
-
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_data_stage_1_ff <= '0;
-    else
+  always_ff @ (posedge gated_clk_0)
+    // if(pow_input_en_0)
       pow_data_stage_1_ff <= pow_mul_stage_1;
+    // else
+      // pow_data_stage_1_ff <= pow_data_stage_1_ff;
 
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_data_stage_2_ff <= '0;
-    else
+  always_ff @ (posedge gated_clk_1)
+    // if(pow_input_en_1)
       pow_data_stage_2_ff <= pow_mul_stage_2;
+    // else
+      // pow_data_stage_2_ff <= pow_data_stage_2_ff;  
 
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_data_stage_3_ff <= '0;
-    else
+  always_ff @ (posedge gated_clk_2)
+    // if(pow_input_en_2)
       pow_data_stage_3_ff <= pow_mul_stage_3;
+    // else
+      // pow_data_stage_3_ff <= pow_data_stage_3_ff;
 
-  always_ff @ (posedge clk_i or posedge rst_i)
-    if (rst_i)
-      pow_output_ff <= '0;
-    else
+  always_ff @ (posedge gated_clk_3)
+    // if(pow_input_en_3)
       pow_output_ff <= pow_mul_stage_4;
-
-
+    // else
+      // po w_output_ff <= pow_output_ff;
+  
   assign pow_data_o   = pow_output_ff;
   assign data_valid_o = output_valid_ff;
 
